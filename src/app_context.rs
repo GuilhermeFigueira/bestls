7319a0804c9b04d473438458d92c2050276e5184
Guilherme Figueira <guibf2004@gmail.com>
2026-07-23 14:44:02 -0300

    feat: criar configuração e contexto da aplicação

---
+++ src/app_context.rs
@@ -0,0 +1,46 @@
+mod config;
+use anyhow::{Context, Result};
+use directories::ProjectDirs;
+use std::{fs, path::PathBuf};
+use supports_hyperlinks::Stream;
+
+use crate::app_context::config::Config;
+
+pub struct AppContext {
+    pub(crate) config: config::Config,
+    pub(crate) proj_path: PathBuf,
+    pub(crate) cache_path: PathBuf,
+    pub(crate) supports_hyperlinks: bool,
+}
+
+impl AppContext {
+    pub fn load() -> Result<Self> {
+        let proj_dirs =
+            Self::create_and_return_project_dir().context("Failed to load config file")?;
+        let config = Config::load(proj_dirs.config_dir()).context("Failed to load config")?;
+        Ok(Self {
+            config: config,
+            proj_path: proj_dirs.project_path().to_path_buf(),
+            cache_path: proj_dirs.cache_dir().to_path_buf(),
+            supports_hyperlinks: supports_hyperlinks::on(Stream::Stdout),
+        })
+    }
+
+    fn create_and_return_project_dir() -> Result<ProjectDirs> {
+        let proj_dirs = ProjectDirs::from("com", "", "bestls")
+            .context("Error formulating the project directory")?;
+        fs::create_dir_all(proj_dirs.config_dir()).with_context(|| {
+            format!(
+                "Error creating the project config directory: {:?}",
+                proj_dirs.config_dir()
+            )
+        })?;
+        fs::create_dir_all(proj_dirs.cache_dir()).with_context(|| {
+            format!(
+                "Error creating the project cache directory: {:?}",
+                proj_dirs.config_dir()
+            )
+        })?;
+        Ok(proj_dirs)
+    }
+}
---
+++ src/app_context/config.rs
@@ -0,0 +1,76 @@
+use anyhow::{Context, Result};
+use serde::{Deserialize, Serialize};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
+#[derive(Deserialize, Debug, Serialize)]
+pub(super) struct Config {
+    pub(super) display: Display,
+}
+
+impl Config {
+    pub(super) fn load(config_path: &Path) -> Result<Self> {
+        let config_file_path = PathBuf::from(&config_path.join("config.toml"));
+
+        if fs::exists(&config_file_path).with_context(|| {
+            format!(
+                "Error checking if config file exists: {:?}",
+                &config_file_path
+            )
+        })? {
+            Self::read_existing_config(&config_file_path)
+        } else {
+            Self::write_default_config(&config_file_path)
+        }
+    }
+
+    fn read_existing_config(path: &Path) -> Result<Config> {
+        let content = fs::read_to_string(path)
+            .with_context(|| format!("Error reading config file: {:?}", path))?;
+
+        match toml::from_str(&content) {
+            Ok(config) => Ok(config),
+            Err(e) => {
+                eprintln!("Invalid config file, using default options: {}", e);
+                Ok(Self::default())
+            }
+        }
+    }
+
+    fn write_default_config(path: &Path) -> Result<Config> {
+        let config = Self::default();
+
+        let toml_string = toml::to_string(&config)
+            .with_context(|| format!("Error converting default config to TOML: {:?}", &config))?;
+
+        fs::write(path, toml_string)
+            .with_context(|| format!("Error writing config file: {:?}", path))?;
+
+        Ok(config)
+    }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            display: Display::default(),
+        }
+    }
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+pub(super) struct Display {
+    pub(super) show_hidden: bool,
+    pub(super) show_folder_size: bool,
+}
+
+impl Default for Display {
+    fn default() -> Self {
+        Self {
+            show_hidden: true,
+            show_folder_size: true,
+        }
+    }
+}
